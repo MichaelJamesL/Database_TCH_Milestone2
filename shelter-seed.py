2a931f0bb1a354bbddf2028daa2c460f6afea2b0
utami <suryaniutami449@gmail.com>
2026-05-09 18:34:25 +0700

    feat: shelter

---
+++ shelter-seed.py
@@ -0,0 +1,59 @@
+import random
+
+from faker import Faker
+
+fake = Faker("id_ID")
+
+OUTPUT_FILE = "shelter.sql"
+
+def esc(s: str) -> str:
+    return s.replace("'", "''").replace("\n", " ").strip()
+
+def seed_shelter(f, jumlah: int):
+    prefix = [
+        "Shelter",
+        "Rumah Singgah",
+        "Pusat Adopsi",
+        "Yayasan",
+        "Animal Rescue"
+    ]
+
+    nama_hewan = [
+        "Kucing",
+        "Anjing",
+        "Satwa",
+        "Paws",
+        "Meong",
+        "Pawtopia",
+        "PetCare",
+        "Animalia"
+    ]
+
+    suffix = [
+        "Harapan",
+        "Bahagia",
+        "Nusantara",
+        "Sejahtera",
+        "Bandung",
+        "Indonesia",
+        "Care",
+        "Rescue"
+    ]
+    for _ in range(jumlah):
+        nama = nama = esc(
+            f"{random.choice(prefix)} "
+            f"{random.choice(nama_hewan)} "
+            f"{random.choice(suffix)}"
+        )
+        kota = esc(fake.city())
+        negara = "Indonesia"
+        f.write(f"INSERT INTO shelter (nama, kota, negara) VALUES ('{nama}', '{kota}', '{negara}');\n")
+
+if __name__ == "__main__":
+    N_SHELTER = 30
+
+    with open(OUTPUT_FILE, "a", encoding="utf-8") as f:
+        f.write("-- shelter\n")
+        seed_shelter(f, N_SHELTER)
+
+    print(f"SQL Seed File: {OUTPUT_FILE}")
---
+++ shelter.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS  shelter (
+    id_shelter INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(100) NOT NULL,
+    kota VARCHAR(100) NOT NULL,
+    negara VARCHAR(100) NOT NULL
+);
+
+-- shelter
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Animalia Care', 'Samarinda', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Animal Rescue Paws Bahagia', 'Subulussalam', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Animalia Bahagia', 'Meulaboh', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Meong Rescue', 'Kotamobagu', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Rumah Singgah Anjing Rescue', 'Kota Administrasi Jakarta Barat', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Pusat Adopsi Animalia Bahagia', 'Cilegon', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Rumah Singgah PetCare Bandung', 'Kediri', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Animal Rescue Paws Care', 'Payakumbuh', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter PetCare Rescue', 'Pagaralam', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Rumah Singgah Meong Rescue', 'Surabaya', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Pusat Adopsi Animalia Bandung', 'Sungai Penuh', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Rumah Singgah Meong Indonesia', 'Bitung', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Animal Rescue Kucing Harapan', 'Bandung', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Meong Rescue', 'Ambon', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Pawtopia Harapan', 'Tangerang', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Anjing Sejahtera', 'Banjarbaru', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Animalia Nusantara', 'Palembang', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Pawtopia Sejahtera', 'Tarakan', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Rumah Singgah PetCare Nusantara', 'Pangkalpinang', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Paws Bahagia', 'Kendari', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Yayasan Animalia Sejahtera', 'Bogor', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Rumah Singgah Satwa Care', 'Samarinda', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Animal Rescue Meong Nusantara', 'Langsa', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Animal Rescue Meong Bandung', 'Pematangsiantar', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Pusat Adopsi Satwa Nusantara', 'Ternate', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Pusat Adopsi Paws Sejahtera', 'Surakarta', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Animal Rescue Pawtopia Harapan', 'Batu', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Animal Rescue Paws Care', 'Tomohon', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Shelter Animalia Harapan', 'Pasuruan', 'Indonesia');
+INSERT INTO shelter (nama, kota, negara) VALUES ('Animal Rescue Pawtopia Bahagia', 'Madiun', 'Indonesia');
